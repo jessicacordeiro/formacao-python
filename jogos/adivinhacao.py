cb8455eb429fc1def3dc4d66eea29cb8f135440d
jessicacordeiro <jessica.cordeiro121@gmail.com>
2021-06-06 01:43:49 -0300

    Aplicando variáveis e condições com pistas

--- jogos/adivinhacao.py
+++ jogos/adivinhacao.py
@@ -10,9 +10,16 @@ print("Você digitou ", chute_str)
 
 chute = int(chute_str)
 
-if(numero_secreto == chute):
+acertou = chute == numero_secreto
+maior   = chute > numero_secreto
+menor   = chute < numero_secreto
+
+if(acertou):
     print("Você acertou! *-*")
 else:
-    print("Você errou! T-T ")
+    if(maior):
+        print("Você errou! O seu chute foi maior que o número secreto. T-T")
+    elif(menor):
+        print("Você errou! O seu chute foi menor que o número secreto. T-T")
 
 print("Fim de jogo.")
