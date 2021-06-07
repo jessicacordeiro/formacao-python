d375c135b19181a7ee70164e6bd44bf4866af84b
jessicacordeiro <jessica.cordeiro121@gmail.com>
2021-06-06 23:18:03 -0300

    Colocando o laço while para o jogador obter 3 tentativas

--- jogos/adivinhacao.py
+++ jogos/adivinhacao.py
@@ -3,23 +3,27 @@ print("Bem vindo ao jogo de Adivinhação!")
 print("**********************************")
 
 numero_secreto = 13
+total_tentativas = 3
+rodada = 1
 
-chute_str = input("Digite um número de sua preferência: ")
+while(rodada <= total_tentativas):
+    print("Tentativa {} de {}".format(rodada, total_tentativas))
+    chute_str = input("Digite um número de sua preferência: ")
+    print("Você digitou ", chute_str)
+    chute = int(chute_str)
 
-print("Você digitou ", chute_str)
+    acertou = chute == numero_secreto
+    maior   = chute > numero_secreto
+    menor   = chute < numero_secreto
 
-chute = int(chute_str)
+    if(acertou):
+        print("Você acertou! *-*")
+    else:
+        if(maior):
+            print("Você errou! O seu chute foi maior que o número secreto. T-T")
+        elif(menor):
+            print("Você errou! O seu chute foi menor que o número secreto. T-T")
 
-acertou = chute == numero_secreto
-maior   = chute > numero_secreto
-menor   = chute < numero_secreto
-
-if(acertou):
-    print("Você acertou! *-*")
-else:
-    if(maior):
-        print("Você errou! O seu chute foi maior que o número secreto. T-T")
-    elif(menor):
-        print("Você errou! O seu chute foi menor que o número secreto. T-T")
+    rodada += 1
 
 print("Fim de jogo.")
